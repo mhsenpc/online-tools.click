a851961bb10031fa5207a138509ce9f1ea59f3cf
Mohsen Shamohammadi <mohsen.shamohammadi@atolls.com>
2026-04-13 12:37:02 +0200

    fix base dir

--- diff_checker/vite.config.ts
+++ diff_checker/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/diff_checker/',
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
